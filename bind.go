d9b5f7374dcc5a3ff58289cc58a164a905c29a6c
Kanata <kkanata996@gmail.com>
2026-03-28 22:44:38 +0800

    Remove unused bindInput helper

--- bind.go
+++ bind.go
@@ -7,10 +7,6 @@ import (
 	"github.com/kanata996/chix/internal/inputschema"
 )
 
-func bindInput(r *http.Request, dst any) error {
-	return normalizeBindError(binding.Bind(r, dst))
-}
-
 func bindInputWithSchema(r *http.Request, dst any, schema *inputschema.Schema) error {
 	return normalizeBindError(binding.BindWithSchema(r, dst, schema))
 }
